38876859b508b2357f56068c6077f75f27c39909
benazir1 <benazir1989@gmail.com>
2023-11-03 17:40:38 +0530

    changes

--- src/Components/Login/index.jsx
+++ src/Components/Login/index.jsx
@@ -18,8 +18,8 @@ const Login = () => {
 			const url = "https://fitnessbackend-0wev.onrender.com/api/auth";
 			const { data: res } = await axios.post(url, data);
 			localStorage.setItem("token", res.data);
-			//navigate('/create');
-			window.location = "/create";
+			navigate('/create');
+			//window.location = "/create";
 		} catch (error) {
 			if (
 				error.response &&
